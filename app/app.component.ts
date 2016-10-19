546f6617dc84977215d1185b7aa49b4ce28bf5f3
tranthienbinh1989 <tranthienbinh1989@gmail.com>
2016-10-19 16:21:48 -0500

    Init commit

---
+++ app/app.component.ts
@@ -0,0 +1,99 @@
+import { Component } from '@angular/core';
+
+const products: Product[] = [
+  { id: 11, name: 'Iphone 7S' },
+  { id: 12, name: 'Iphone 6S' },
+  { id: 13, name: 'Ipad Air' },
+  { id: 14, name: 'Macbook Pro' },
+  { id: 15, name: 'Macbook Air' },
+  { id: 16, name: 'Apple TV' }
+];
+
+@Component({
+    selector: 'my-app',
+    styles: [`
+  .selected {
+    background-color: #CFD8DC !important;
+    color: white;
+  }
+  .heroes {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 15em;
+  }
+  .heroes li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .heroes li.selected:hover {
+    background-color: #BBD8DC !important;
+    color: white;
+  }
+  .heroes li:hover {
+    color: #607D8B;
+    background-color: #DDD;
+    left: .1em;
+  }
+  .heroes .text {
+    position: relative;
+    top: -3px;
+  }
+  .heroes .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0 0 4px;
+  }
+`],
+    template:`
+    <h1>{{title}}</h1>
+    <h2>Products avaibale in store</h2>
+    <ul class="heroes">
+      <li *ngFor="let product of products"
+        [class.selected]="product === selectedProduct"
+        (click)="onSelect(product)">
+        <span class="badge">{{product.id}}</span> {{product.name}}
+      </li>
+    </ul>
+    <div *ngIf="selectedProduct">
+      <h2>{{selectedProduct.name}} details!</h2>
+      <div><label>id: </label>{{selectedProduct.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="selectedProduct.name" placeholder="name"/>
+      </div>
+    </div>
+  `
+})
+export class AppComponent {
+    title = 'Apple Store';
+    product : Product = {
+                id: 1,
+                name: 'Macbook'
+            };
+    products = products;
+    selectedProduct: Product;
+    onSelect(product: Product): void {
+        this.selectedProduct = product;
+        }
+}
+
+export class Product {
+  id: number;
+  name: string;
+}
